8d9c51327b05e9ca43594401a7404908b6d0e7ee
artbodrov <artem.bodrov@gmail.com>
2023-08-16 19:23:15 +0300

    add differ

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -2,7 +2,7 @@ package hexlet.code;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
-        return generate(filePath1, filePath2);
+        return generate(filePath1, filePath2 );
     }
 
 }
