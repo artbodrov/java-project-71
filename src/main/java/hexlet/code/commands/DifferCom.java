1af69cea8a7a59aaf234ee61e8c043e8bb4176f7
artbodrov <artem.bodrov@gmail.com>
2023-08-16 20:46:36 +0300

    add

---
+++ src/main/java/hexlet/code/commands/DifferCom.java
@@ -0,0 +1,41 @@
+package hexlet.code.commands;
+
+import hexlet.code.Differ;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.Command;
+import static picocli.CommandLine.Option;
+import static picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.")
+public final class DifferCom implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"},
+            defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            paramLabel = "format")
+    private String format;
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filePath1")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filePath2")
+    private String filePath2;
+
+    @Override
+    public Integer call() {
+        try {
+            String diff = Differ.generate(filePath1, filePath2, format);
+            System.out.println(diff);
+            return 0;
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+            return 1;
+        }
+    }
+}
